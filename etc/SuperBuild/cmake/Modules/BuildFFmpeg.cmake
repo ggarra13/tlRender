633e4135cc6caf7c4facaecd507764a76d2e41bc
Gonzalo Garramuño <ggarra13@gmail.com>
2023-01-29 07:38:48 -0300

    Updated FFmpeg for libvpx codec.

--- etc/SuperBuild/cmake/Modules/BuildFFmpeg.cmake
+++ etc/SuperBuild/cmake/Modules/BuildFFmpeg.cmake
@@ -8,15 +8,18 @@ set(FFmpeg_DEBUG OFF)
 if(WIN32)
     # See the directions for building FFmpeg on Windows in "docs/build_windows.html".
 else()
+    set(FFmpeg_CONFIGURE)
     set(FFmpeg_CFLAGS)
     set(FFmpeg_CXXFLAGS)
     set(FFmpeg_OBJCFLAGS)
     set(FFmpeg_LDFLAGS)
-    if(APPLE AND CMAKE_OSX_DEPLOYMENT_TARGET)
-        list(APPEND FFmpeg_CFLAGS "--extra-cflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
-        list(APPEND FFmpeg_CXXFLAGS "--extra-cxxflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
-        list(APPEND FFmpeg_OBJCFLAGS "--extra-objcflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
-        list(APPEND FFmpeg_LDFLAGS "--extra-ldflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+    if(APPLE)
+        if (CMAKE_OSX_DEPLOYMENT_TARGET)
+            list(APPEND FFmpeg_CFLAGS "--extra-cflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+            list(APPEND FFmpeg_CXXFLAGS "--extra-cxxflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+            list(APPEND FFmpeg_OBJCFLAGS "--extra-objcflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+            list(APPEND FFmpeg_LDFLAGS "--extra-ldflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+        endif()
     endif()
     if(FFmpeg_DEBUG)
         list(APPEND FFmpeg_CFLAGS "--extra-cflags=-g")
@@ -36,12 +39,16 @@ else()
         --disable-audiotoolbox
         --disable-vaapi
         --disable-sdl2
+        --enable-libvpx
         --enable-pic
         ${FFmpeg_CFLAGS}
         ${FFmpeg_CXXFLAGS}
         ${FFmpeg_OBJCFLAGS}
         ${FFmpeg_LDFLAGS}
         --x86asmexe=${CMAKE_INSTALL_PREFIX}/bin/nasm)
+    if (APPLE AND CMAKE_OSX_ARCHITECTURES )
+        list(APPEND FFmpeg_CONFIGURE_ARGS --arch=${CMAKE_OSX_ARCHITECTURES} )
+    endif()
     if(UNIX)
         list(APPEND FFmpeg_CONFIGURE_ARGS
             --disable-libxcb
