7eb0f8c075ea8d28a40ddabd6b7129374a26dc3a
Gonzalo Garramuño <ggarra13@gmail.com>
2023-02-10 00:11:39 -0300

    Added --prefix to configure command.

--- etc/SuperBuild/cmake/Modules/BuildYASM.cmake
+++ etc/SuperBuild/cmake/Modules/BuildYASM.cmake
@@ -7,7 +7,7 @@ ExternalProject_Add(
   YASM
   PREFIX ${CMAKE_CURRENT_BINARY_DIR}/YASM
   URL "http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz"
-  CONFIGURE_COMMAND ./configure
+  CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX}
   BUILD_IN_SOURCE 1
   )
 
