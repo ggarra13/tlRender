b0ce5586d612200ea0c41c265886db13b6b47908
Gonzalo Garramuño <ggarra13@gmail.com>
2023-02-10 00:32:13 -0300

    Changed to configure to build YASM.

--- etc/SuperBuild/cmake/Modules/BuildYASM.cmake
+++ etc/SuperBuild/cmake/Modules/BuildYASM.cmake
@@ -4,13 +4,16 @@ include( ExternalProject )
 # GIT_REPOSITORY "https://github.com/yasm/yasm.git"
 
 ExternalProject_Add(
-  YASM
-  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/YASM
-  URL "http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz"
-  CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX}
-  BUILD_IN_SOURCE 1
-  )
+    YASM
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/YASM
+    URL "http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz"
+    CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX}
+    BUILD_IN_SOURCE 1
+)
 
+#
+# Do not use the cmake interfacte as it requires python (aargh!)
+#
 # ExternalProject_Add(
 #   YASM
 #   PREFIX ${CMAKE_CURRENT_BINARY_DIR}/YASM
