cf26eb0dfa34e766422ccc27d237b6e37035b83c
Gonzalo Garramuño <ggarra13@gmail.com>
2023-09-20 23:46:39 -0300

    Build fix.

--- cmake/Modules/FindLibRaw.cmake
+++ cmake/Modules/FindLibRaw.cmake
@@ -1,9 +1,9 @@
-# Find the LibRaw library.
+# Find the libraw library.
 #
 # This module defines the following variables:
 #
-# * LibRaw_INCLUDE_DIRS
-# * LibRaw_LIBRARIES
+# * LIBRAW_INCLUDE_DIRS
+# * LIBRAW_LIBRARIES
 #
 # This module defines the following imported targets:
 #
@@ -15,35 +15,37 @@
 
 find_package(ZLIB REQUIRED)
 
-find_path(LibRaw_INCLUDE_DIR NAMES libraw/libraw.h)
-set(LibRaw_INCLUDE_DIRS
-    ${LibRaw_INCLUDE_DIR}
+find_path(LIBRAW_INCLUDE_DIR NAMES libraw.h)
+set(LIBRAW_INCLUDE_DIRS
+    ${LIBRAW_INCLUDE_DIR}
     ${ZLIB_INCLUDE_DIRS})
 
-find_library(LibRaw_libraw_LIBRARY NAMES raw)
-set(LibRaw_LIBRARIES
-    ${LibRaw_libraw_LIBRARY}
+if(CMAKE_BUILD_TYPE MATCHES "^Debug$")
+    find_library(LIBRAW_LIBRARY
+        NAMES libraw16d liblibraw16d liblibraw16_staticd librawd libraw16 libraw16_static liblibraw16_static libraw)
+else()
+    find_library(LIBRAW_LIBRARY
+        NAMES libraw16 liblibraw16 libraw16_static liblibraw16_static libraw)
+endif()
+set(LIBRAW_LIBRARIES
+    ${LIBRAW_LIBRARY}
     ${ZLIB_LIBRARIES})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-    LibRaw
-    REQUIRED_VARS
-        LibRaw_INCLUDE_DIR
-        LibRaw_libraw_LIBRARY)
+    LIBRAW
+    REQUIRED_VARS LIBRAW_INCLUDE_DIR LIBRAW_LIBRARY)
+mark_as_advanced(LIBRAW_INCLUDE_DIR LIBRAW_LIBRARY)
 
-mark_as_advanced(
-    LibRaw_INCLUDE_DIR
-    LibRaw_libraw_LIBRARY)
-
-if(LibRaw_FOUND AND NOT TARGET LibRaw::libraw)
+if(LIBRAW_FOUND AND NOT TARGET LibRaw::libraw)
     add_library(LibRaw::libraw UNKNOWN IMPORTED)
-    set(LibRaw_libraw_LINK_LIBRARIES LibRaw::libraw ZLIB)
     set_target_properties(LibRaw::libraw PROPERTIES
-        IMPORTED_LOCATION "${LibRaw_libraw_LIBRARY}"
-        INTERFACE_LINK_LIBRARIES "${LibRaw_libraw_LINK_LIBRARIES}")
+        IMPORTED_LOCATION "${LIBRAW_LIBRARY}"
+        INTERFACE_COMPILE_DEFINITIONS LIBRAW_FOUND
+        INTERFACE_INCLUDE_DIRECTORIES "${LIBRAW_INCLUDE_DIR}"
+        INTERFACE_LINK_LIBRARIES "ZLIB")
 endif()
-if(LibRaw_FOUND AND NOT TARGET LibRaw)
-    add_library(LibRaw INTERFACE)
-    target_link_libraries(LibRaw INTERFACE LibRaw::libraw)
+if(LIBRAW_FOUND AND NOT TARGET LIBRAW)
+    add_library(LIBRAW INTERFACE)
+    target_link_libraries(LIBRAW INTERFACE LibRaw::libraw)
 endif()
