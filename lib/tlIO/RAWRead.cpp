dd25b7f909057c5798fe61b9d45e99a6f1397345
Gonzalo Garramuño <ggarra13@gmail.com>
2023-09-21 11:04:22 -0300

    Reverted RtAudio patches.

--- lib/tlIO/RAWRead.cpp
+++ lib/tlIO/RAWRead.cpp
@@ -14,7 +14,7 @@ namespace tl
     {
         namespace
         {
-            
+
             class File
             {
             public:
@@ -31,9 +31,16 @@ namespace tl
                     }
                     else
                     {
+                        _info.video.resize(1);
+                        auto& info = _info.video[0];
+                        iProcessor.imgdata.params.output_bps = 16;
 
                         // Open the file and read the metadata
+#ifdef _WIN32
                         int ret = iProcessor.open_file(fileName.c_str());
+#else
+                        int ret = iProcessor.open_file(fileName.c_str());
+#endif
                         if (ret != 0)
                         {
                             throw std::runtime_error(
@@ -42,23 +49,42 @@ namespace tl
                                 .arg("open_file failed")
                                 .arg(ret));
                         }
-                        
-                        _info.size.w = iProcessor.imgdata.sizes.width;
-                        _info.size.h = iProcessor.imgdata.sizes.height;
-                    
-                        _info.pixelType = image::PixelType::RGBA_U8;
-                        if (image::PixelType::None == _info.pixelType)
+
+                        const libraw_iparams_t& idata(iProcessor.imgdata.idata);
+                        const libraw_image_sizes_t& sizes(iProcessor.imgdata.sizes);
+                        switch(sizes.flip)
                         {
-                            throw std::runtime_error(
-                                string::Format("{0}: {1}")
-                                    .arg(fileName)
-                                    .arg("Unsupported image type"));
+                        case 5:
+                            info.layout.mirror.y = true;
+                        case 6:
+                            info.size.h = sizes.width;
+                            info.size.w = sizes.height;
+                            break;
+                        case 0: // no rotation
+                            info.layout.mirror.y = true;
+                        case 3:
+                        default:
+                            info.size.w = sizes.width;
+                            info.size.h = sizes.height;
+                            break;
                         }
-                        _info.layout.endian = memory::Endian::MSB;
+
+                        // Save Tags
+                        auto& tags = _info.tags;
+                        {
+                            std::stringstream ss;
+                            ss << sizes.flip;
+                            tags["flip"] = ss.str();
+                        }
+                        tags["Camera Manufacturer"] = idata.make;
+                        tags["Camera Model"] = idata.model;
+                        tags["Software"] = idata.software;
+                        info.pixelType = image::PixelType::RGB_U16;
+                        info.layout.endian = memory::Endian::LSB;
                     }
                 }
 
-                const image::Info& getInfo() const
+                const io::Info& getInfo() const
                 {
                     return _info;
                 }
@@ -69,10 +95,27 @@ namespace tl
                 {
                     io::VideoData out;
                     out.time = time;
-                    out.image = image::Image::create(_info);
+                    const auto& info = _info.video[0];
+                    out.image = image::Image::create(info);
 
+#ifdef _WIN32
+                    int ret = iProcessor.open_file(fileName.c_str());
+#else
+                    int ret = iProcessor.open_file(fileName.c_str());
+#endif
+                    if (ret != 0)
+                    {
+                        throw std::runtime_error(
+                            string::Format("{0}: {1} - Error {2}")
+                            .arg(fileName)
+                            .arg("open_file failed")
+                            .arg(ret));
+                    }
+                    
+                    const libraw_image_sizes_t& sizes(iProcessor.imgdata.sizes);
+                        
                     // Let us unpack the image
-                    int ret = iProcessor.unpack();
+                    ret = iProcessor.unpack();
                     if (ret != 0)
                     {
                         throw std::runtime_error(
@@ -82,33 +125,49 @@ namespace tl
                             .arg(ret));
                     }
                     
-                    ret = iProcessor.raw2image();
+                    ret = iProcessor.dcraw_process();
                     if (ret != 0)
                     {
                         throw std::runtime_error(
                             string::Format("{0}: {1} - Error {2}")
                             .arg(fileName)
-                            .arg("raw2image failed")
+                            .arg("dcraw_process failed")
                             .arg(ret));
                     }
                     
-                    const int channels = image::getChannelCount(_info.pixelType);
-                    const size_t bytes = image::getBitDepth(_info.pixelType) / 8;
-                                                       
-                    memcpy(
-                        out.image->getData(), iProcessor.imgdata.image,
-                        iProcessor.imgdata.sizes.iwidth *
-                        iProcessor.imgdata.sizes.iheight * channels * bytes);
+                    const int channels = image::getChannelCount(info.pixelType);
+                    const size_t bytes = image::getBitDepth(info.pixelType) / 8;
 
+                    image = iProcessor.dcraw_make_mem_image(&ret);
+                    if (ret != 0 || !image)
+                    {
+                        throw std::runtime_error(
+                            string::Format("{0}: {1} - Error {2}")
+                            .arg(fileName)
+                            .arg("dcraw_make_mem_image")
+                            .arg(ret));
+                    }
+
+                    if (image->type != LIBRAW_IMAGE_BITMAP)
+                    {
+                        throw std::runtime_error(
+                            string::Format("{0}: {1}")
+                            .arg(fileName)
+                            .arg("Not a bitmap image"));
+                    }
+
+                    memcpy(out.image->getData(), image->data, image->data_size);
+
+                    iProcessor.dcraw_clear_mem(image);
                     iProcessor.recycle();
-
 
                     return out;
                 }
 
             private:
                 LibRaw iProcessor;
-                image::Info _info;
+                libraw_processed_image_t* image = nullptr; 
+                io::Info _info;
                 const file::MemoryRead* _memory;
             };
         }
@@ -158,8 +217,7 @@ namespace tl
             const std::string& fileName,
             const file::MemoryRead* memory)
         {
-            io::Info out;
-            out.video.push_back(File(fileName, memory).getInfo());
+            io::Info out = File(fileName, memory).getInfo();
             out.videoTime = otime::TimeRange::range_from_start_end_time_inclusive(
                 otime::RationalTime(_startFrame, _defaultSpeed),
                 otime::RationalTime(_endFrame, _defaultSpeed));
