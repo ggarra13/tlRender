80872a6410135e3a8c29769dcebf1b25db3db0cd
Gonzalo Garramuño <ggarra13@gmail.com>
2023-09-28 04:27:37 -0300

    Build fix.

--- etc/SuperBuild/cmake/Modules/BuildLibRaw.cmake
+++ etc/SuperBuild/cmake/Modules/BuildLibRaw.cmake
@@ -33,8 +33,11 @@ set(LibRaw_ARGS
     -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
 )
 
+#
+# This copies the .cmake files from Libraw-cmake.  Two files need patching thou.
+#
 set(LibRaw_PATCH
-    ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_BINARY_DIR}/LibRaw_cmake/src/LibRaw_cmake/CMakeLists.txt <SOURCE_DIR> &&
+    ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/LibRaw-patch/CMakeLists.txt <SOURCE_DIR> &&
     ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_BINARY_DIR}/LibRaw_cmake/src/LibRaw_cmake/cmake <SOURCE_DIR>/cmake &&
     ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/LibRaw-patch/FindLCMS2.cmake <SOURCE_DIR>/cmake/modules
 )
