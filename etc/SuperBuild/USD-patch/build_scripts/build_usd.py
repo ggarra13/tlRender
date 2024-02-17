b0f6800f5c31db95686e7f556da29963e94e1bfd
Gonzalo Garramuño <ggarra13@gmail.com>
2024-02-17 08:26:49 -0300

    Use curl in insecure mode.

--- etc/SuperBuild/USD-patch/build_scripts/build_usd.py
+++ etc/SuperBuild/USD-patch/build_scripts/build_usd.py
@@ -502,7 +502,7 @@ def PatchFile(filename, patches, multiLineMatches=False):
 def DownloadFileWithCurl(url, outputFilename):
     # Don't log command output so that curl's progress
     # meter doesn't get written to the log file.
-    Run("curl {progress} -L -o {filename} {url}".format(
+    Run("curl -k {progress} -L -o {filename} {url}".format(
         progress="-#" if verbosity >= 2 else "-s",
         filename=outputFilename, url=url), 
         logCommandOutput=False)
