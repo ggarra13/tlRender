3033bfd5b36f4fed4e7449df17f2a50612e5196c
Gonzalo Garramuño <ggarra13@gmail.com>
2024-02-17 03:13:59 -0300

    Set macOS flags for dav1d codec compilation.

--- etc/SuperBuild/cmake/Modules/Builddav1d.cmake
+++ etc/SuperBuild/cmake/Modules/Builddav1d.cmake
@@ -3,7 +3,7 @@ include(ExternalProject)
 
 set(dav1d_GIT_TAG 1.3.0)
 
-set(dav1d_CONFIGURE ${CMAKE_COMMAND} -E env PYTHONPATH="" -- meson setup -Denable_tools=false -Denable_tests=false --default-library=static -Dlibdir=${CMAKE_INSTALL_PREFIX}/lib --prefix=${CMAKE_INSTALL_PREFIX} build)
+set(dav1d_CONFIGURE ${CMAKE_COMMAND} -E env PYTHONPATH="" "CXXFLAGS=${CMAKE_CXX_FLAGS}" "CFLAGS=${CMAKE_C_FLAGS}" "LDFLAGS=${CMAKE_SHARED_LINKER_FLAGS}" -- meson setup -Denable_tools=false -Denable_tests=false --default-library=static -Dlibdir=${CMAKE_INSTALL_PREFIX}/lib --prefix=${CMAKE_INSTALL_PREFIX} build)
 set(dav1d_BUILD export PYTHONPATH="" && cd build && ninja)
 set(dav1d_INSTALL export PYTHONPATH="" && cd build && ninja install)
 
