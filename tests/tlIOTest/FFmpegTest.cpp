4ee9387cef678ffe59e9580d56243d984cfcfe4f
Gonzalo Garramuño <ggarra13@gmail.com>
2024-02-25 19:51:01 -0300

    Revert "Changed .mp4 container to .mkv one."
    This reverts commit 9367b484c13598ef667fd58185e601cfce0ec2d3.

--- tests/tlIOTest/FFmpegTest.cpp
+++ tests/tlIOTest/FFmpegTest.cpp
@@ -207,7 +207,7 @@ namespace tl
                                     file::Path path;
                                     {
                                         std::stringstream ss;
-                                        ss << fileName << '_' << size << '_' << pixelType << ".mkv";
+                                        ss << fileName << '_' << size << '_' << pixelType << ".mp4";
                                         _print(ss.str());
                                         path = file::Path(ss.str());
                                     }
