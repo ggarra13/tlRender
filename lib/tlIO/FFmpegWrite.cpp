2899ac702f845e50705b2ae2f9a71dce8066b40c
Gonzalo Garramuño <ggarra13@gmail.com>
2024-02-25 19:01:14 -0300

    Hard-coded allow_sw 1 so that github tests don't fail.

--- lib/tlIO/FFmpegWrite.cpp
+++ lib/tlIO/FFmpegWrite.cpp
@@ -358,6 +358,10 @@ namespace tl
                 }
                 file.close();
 
+#ifdef __APPLE__
+                av_dict_set(&codecOptions, "allow_sw", "1", 0);
+#endif
+                    
                 for (const auto& pair : settings)
                 {
                     av_dict_set(&codecOptions, pair.first.c_str(),
