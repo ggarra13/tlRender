9245ef07f7642d5781c790b800ea189cf0964035
Gonzalo Garramuño <ggarra13@gmail.com>
2024-03-11 15:34:42 -0300

    Reverted coeffs to Darby's.

--- lib/tlCore/Image.cpp
+++ lib/tlCore/Image.cpp
@@ -93,9 +93,7 @@ namespace tl
             const std::array<math::Vector4f, static_cast<size_t>(YUVCoefficients::Count)> data =
             {
                 math::Vector4f(1.79274, 2.1124, 0.213242, 0.532913),
-                // Is Darby's BT2020 right?  These are the coeffs I got:
-                math::Vector4f(1.8556, 2.14101, 0.2124, 0.6301), // gemini
-                //math::Vector4f(1.67867, 2.14177, 0.187332, 0.650421) // darby
+                math::Vector4f(1.67867, 2.14177, 0.187332, 0.650421),
             };
             return data[static_cast<size_t>(value)];
         }
