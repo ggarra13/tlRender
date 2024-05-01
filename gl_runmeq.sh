38e4aba2e2dd42c92df6f885520509d64ac52265
Gonzalo Garramuño <ggarra13@gmail.com>
2024-05-01 20:13:48 -0300

    Build fixes.

---
+++ gl_runmeq.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# mrv2 
+# Copyright Contributors to the mrv2 Project. All rights reserved.
+
+
+#
+# Turn on exit on error
+#
+set -o pipefail -e
+
+export BUILD_DIR=_build
+
+rm -f $BUILD_DIR/tlRender/src/tlRender-build/bin/tlplay-gl/tlplay-gl
+
+cd $BUILD_DIR/tlRender/src/tlRender-build
+
+cmake --build . --config Release -t install
+
+cd -
+
