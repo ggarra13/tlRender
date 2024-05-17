293776bc26e07d4e9cefc750e3e38210c9258cd4
Gonzalo Garramuño <ggarra13@gmail.com>
2024-05-17 12:50:54 -0300

    Changed BuildOTIO.cmake.

--- etc/SuperBuild/cmake/Modules/BuildOTIO.cmake
+++ etc/SuperBuild/cmake/Modules/BuildOTIO.cmake
@@ -1,8 +1,9 @@
 include(ExternalProject)
 
-set(OTIO_GIT_REPOSITORY "https://github.com/ggarra13/OpenTimelineIO.git")
-set(OTIO_GIT_TAG "edit_commands")  # stable branch
-
+# set(OTIO_GIT_REPOSITORY "https://github.com/ggarra13/OpenTimelineIO.git")
+# set(OTIO_GIT_TAG "edit_commands")  # stable branch
+set(OTIO_GIT_REPOSITORY "https://github.com/AcademySoftwareFoundation/OpenTimelineIO.git")
+set(OTIO_GIT_TAG "v0.16.0")
 
 set(OTIO_SHARED_LIBS ON)
 if(NOT BUILD_SHARED_LIBS)
