b1f017d3e9c51c3ae1969c8a2db3023e898f77f2
Gonzalo Garramuño <ggarra13@gmail.com>
2024-06-08 09:32:05 -0300

    Updated Buildglfw3.cmake to darby's version as it is newer than 3.4.

--- etc/SuperBuild/cmake/Modules/Buildglfw3.cmake
+++ etc/SuperBuild/cmake/Modules/Buildglfw3.cmake
@@ -1,22 +1,12 @@
 include(ExternalProject)
 
 set(glfw3_GIT_REPOSITORY "https://github.com/glfw/glfw.git")
-#set(glfw3_GIT_TAG 3eaf1255b29fdf5c2895856c7be7d7185ef2b241)  # used to work
-set(glfw3_GIT_TAG "3.4")  # used to work
+set(glfw3_GIT_TAG "3eaf1255b29fdf5c2895856c7be7d7185ef2b241")
 
-set(glfw3_Linux_ARGS )
-if(UNIX AND NOT APPLE)
-    list(APPEND glfw3_Linux_ARGS
-	-DGLFW_BUILD_X11=${TLRENDER_X11}
-	-DGLFW_BUILD_WAYLAND=${TLRENDER_WAYLAND}
-    )
-endif()
-
-set(glfw3_PATCH )
-#list(APPEND glfw3_PATCH
-#     ${CMAKE_COMMAND} -E copy_if_different
-#     ${CMAKE_CURRENT_SOURCE_DIR}/glfw3-patch/src/nsgl_context.m
-#     ${CMAKE_CURRENT_BINARY_DIR}/glfw3/src/glfw3/src/nsgl_context.m)
+set(glfw3_PATCH
+    ${CMAKE_COMMAND} -E copy_if_different
+    ${CMAKE_CURRENT_SOURCE_DIR}/glfw3-patch/src/nsgl_context.m
+    ${CMAKE_CURRENT_BINARY_DIR}/glfw3/src/glfw3/src/nsgl_context.m)
 if(TLRENDER_GLFW_DISABLE_MACOS_APP_DELEGATE)
     list(APPEND glfw3_PATCH
         ${CMAKE_COMMAND} -E copy_if_different
@@ -26,8 +16,8 @@ endif()
 
 set(glfw3_ARGS
     ${TLRENDER_EXTERNAL_ARGS}
-    ${glfw3_Linux_ARGS}
     -DCMAKE_INSTALL_LIBDIR=lib
+    -DGLFW_SHARED_LIBS=${BUILD_SHARED_LIBS}
     -DGLFW_BUILD_EXAMPLES=FALSE
     -DGLFW_BUILD_TESTS=FALSE
     -DGLFW_BUILD_DOCS=FALSE)
