e9225c98b4025fdb82133333dc49815a0b243c7b
Gonzalo Garramuño <ggarra13@gmail.com>
2024-07-17 15:00:46 -0300

    Added buildlibsnappy.cmake.

---
+++ etc/SuperBuild/cmake/Modules/Buildlibsnappy.cmake
@@ -0,0 +1,31 @@
+include(ExternalProject)
+
+
+set(libsnappy_REPO "https://github.com/google/snappy.git")
+set(libsnappy_TAG 1.2.1)
+
+
+set(libsnappy_ARGS ${TLRENDER_EXTERNAL_ARGS})
+
+list(APPEND libsnappy_ARGS
+    -DBENCHMARK_ENABLE_INSTALL=OFF
+    -DBENCHMARK_INSTALL_DOCS=OFF
+    -DBENCHMARK_USE_BUNDLED_GTEST=OFF
+)
+
+set(libsnappy_UPDATE_CMD "git submodule update --init")
+
+
+ExternalProject_Add(
+    libsnappy
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/libsnappy
+    GIT_REPOSITORY ${libsnappy_REPO}
+    GIT_TAG ${libsnappy_TAG}
+    GIT_SHALLOW 1
+
+    UPDATE_COMMAND ${libsnappy_UPDATE_CMD}
+    
+    LIST_SEPARATOR |
+    CMAKE_ARGS ${libsnappy_ARGS}
+)
+
