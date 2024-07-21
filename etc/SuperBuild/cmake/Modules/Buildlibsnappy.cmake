7f5cc4c5609895d48bfc30137c8433e0d79ca3f7
Gonzalo Garramuño <ggarra13@gmail.com>
2024-07-21 02:10:30 -0300

    Made libsnappy use -fPIC.

--- etc/SuperBuild/cmake/Modules/Buildlibsnappy.cmake
+++ etc/SuperBuild/cmake/Modules/Buildlibsnappy.cmake
@@ -7,10 +7,23 @@ set(libsnappy_TAG 1.2.1)
 
 set(libsnappy_ARGS ${TLRENDER_EXTERNAL_ARGS})
 
+
+if (UNIX)
+    set(libsnappy_CXX_FLAGS -fPIC ${CMAKE_CXX_FLAGS})
+    set(libsnappy_C_FLAGS -fPIC ${CMAKE_C_FLAGS})
+else()
+    set(libsnappy_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+    set(libsnappy_C_FLAGS ${CMAKE_C_FLAGS})
+endif()
+
 list(APPEND libsnappy_ARGS
     -DBENCHMARK_ENABLE_INSTALL=OFF
     -DBENCHMARK_INSTALL_DOCS=OFF
     -DBENCHMARK_USE_BUNDLED_GTEST=OFF
+    -DSNAPPY_BUILD_TESTS=OFF
+    -DSNAPPY_BUILD_BENCHMARKS=OFF
+    -DCMAKE_CXX_FLAGS="${libsnappy_CXX_FLAGS}"
+    -DCMAKE_C_FLAGS="${libsnappy_C_FLAGS}"
 )
 
 set(libsnappy_UPDATE_CMD git submodule update --init)
