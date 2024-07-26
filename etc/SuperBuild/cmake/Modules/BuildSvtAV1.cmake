d5242af623f062492ce2ed2d8816f9c5ec9057b4
Gonzalo Garramuño <ggarra13@gmail.com>
2024-07-26 09:36:30 -0300

    Made Nasm dependency linux only.

--- etc/SuperBuild/cmake/Modules/BuildSvtAV1.cmake
+++ etc/SuperBuild/cmake/Modules/BuildSvtAV1.cmake
@@ -8,13 +8,18 @@ set(SvtAV1_ARGS ${TLRENDER_EXTERNAL_ARGS})
 list(APPEND SvtAV1_ARGS
     -DENABLE_NASM=ON
     -DCMAKE_INSTALL_LIBDIR=lib
-    )
+)
+
+set(SvtAV1_DEPS )
+if(UNIX)
+    set(SvtAV1_DEPS NASM)
+endif()
 
 
 ExternalProject_Add(
     SvtAV1
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/SvtAV1
-    DEPENDS NASM
+    DEPENDS ${SvtAV1_DEPS}
     GIT_REPOSITORY "https://gitlab.com/AOMediaCodec/SVT-AV1.git"
     GIT_TAG ${SvtAV1_TAG}
     GIT_SHALLOW 1
