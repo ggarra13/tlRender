cbaafe6cfb606e6cae04880ebaab6ae264795b34
Gonzalo Garramuño <ggarra13@gmail.com>
2024-10-31 19:42:05 -0300

    Fixed meson installation.

--- etc/SuperBuild/cmake/Modules/Builddav1d.cmake
+++ etc/SuperBuild/cmake/Modules/Builddav1d.cmake
@@ -3,20 +3,24 @@ include(ExternalProject)
 
 set(dav1d_GIT_TAG 1.3.0)
 
+set(dav1d_DEPS NASM ${PYTHON_DEP})
+
 if(APPLE AND CMAKE_OSX_DEPLOYMENT_TARGET)
     set(dav1d_CFLAGS -mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET})
     set(dav1d_CXXFLAGS -mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET})
     set(dav1d_LDFLAGS -mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET})
 endif()
 
-set(dav1d_CONFIGURE ${CMAKE_COMMAND} -E env PYTHONPATH="" "CXXFLAGS=${dav1d_CXXFLAGS}" "CFLAGS=${dav1d_CFLAGS}" "LDFLAGS=${dav1d_LDFLAGS}" -- meson setup -Denable_tools=false -Denable_tests=false --default-library=static -Dlibdir=${CMAKE_INSTALL_PREFIX}/lib --prefix=${CMAKE_INSTALL_PREFIX} build)
+set(dav1d_CONFIGURE
+    COMMAND ${CMAKE_COMMAND} -E -- ${CMAKE_INSTALL_PREFIX}/install/bin/python -m pip install meson
+    COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH="" "CXXFLAGS=${dav1d_CXXFLAGS}" "CFLAGS=${dav1d_CFLAGS}" "LDFLAGS=${dav1d_LDFLAGS}" -- meson setup -Denable_tools=false -Denable_tests=false --default-library=static -Dlibdir=${CMAKE_INSTALL_PREFIX}/lib --prefix=${CMAKE_INSTALL_PREFIX} build)
 set(dav1d_BUILD export PYTHONPATH="" && cd build && ninja)
 set(dav1d_INSTALL export PYTHONPATH="" && cd build && ninja install)
 
 ExternalProject_Add(
     dav1d
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/dav1d
-    DEPENDS NASM
+    DEPENDS ${dav1d_DEPS}
     GIT_REPOSITORY "https://code.videolan.org/videolan/dav1d.git"
     GIT_TAG ${dav1d_GIT_TAG}
     GIT_SHALLOW 1
