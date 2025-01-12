57a4952113df84965e104ff15e60c89071b6a6a8
Gonzalo Garramuño <ggarra13@gmail.com>
2025-01-12 14:48:14 -0300

    Fixed space typo.

--- etc/SuperBuild/cmake/Modules/BuildFFmpeg.cmake
+++ etc/SuperBuild/cmake/Modules/BuildFFmpeg.cmake
@@ -510,8 +510,7 @@ if(WIN32)
     if(WIN32)
 	convert_path_for_msys2("${CMAKE_CURRENT_BINARY_DIR}" BINARY_DIR)
         set(FFmpeg_OPENSSL_COPY
-            "cp ${BINARY_DIR}/ffmpeg_configure.sh.in
-            ${BINARY_DIR}/FFmpeg/src/FFmpeg/ffmpeg_configure.sh &&")
+            "cp ${BINARY_DIR}/ffmpeg_configure.sh.in ${BINARY_DIR}/FFmpeg/src/FFmpeg/ffmpeg_configure.sh &&")
     endif()
 
     set(PKG_CONFIG_PATH_MSys2 "${INSTALL_PREFIX}/lib/pkgconfig")
