007f98e328028216a4eec2d2f59f90780250597b
Gonzalo Garramuño <ggarra13@gmail.com>
2025-03-08 15:05:41 -0300

    Removed PYTHONPATH="".

--- etc/SuperBuild/cmake/Modules/Buildlibplacebo.cmake
+++ etc/SuperBuild/cmake/Modules/Buildlibplacebo.cmake
@@ -27,7 +27,7 @@ endif()
 
 set(libplacebo_CONFIGURE
     COMMAND ${libplacebo_ENV} git submodule update --init
-    COMMAND ${CMAKE_COMMAND} -E env ${CLANG_ENV} "DYLD_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/lib" PYTHONPATH="" "CXXFLAGS=${libplacebo_CXXFLAGS}" "CFLAGS=${libplacebo_CFLAGS}" "LDFLAGS=${libplacebo_LDFLAGS}" -- meson setup --wipe -Dvulkan=disabled -Ddemos=false -Dshaderc=disabled -Dlcms=disabled -Dglslang=disabled -Dlibdir=${CMAKE_INSTALL_PREFIX}/lib --prefix=${CMAKE_INSTALL_PREFIX} build)
+    COMMAND ${CMAKE_COMMAND} -E env ${CLANG_ENV} "DYLD_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/lib" "CXXFLAGS=${libplacebo_CXXFLAGS}" "CFLAGS=${libplacebo_CFLAGS}" "LDFLAGS=${libplacebo_LDFLAGS}" -- meson setup --wipe -Dvulkan=disabled -Ddemos=false -Dshaderc=disabled -Dlcms=disabled -Dglslang=disabled -Dlibdir=${CMAKE_INSTALL_PREFIX}/lib --prefix=${CMAKE_INSTALL_PREFIX} build)
 set(libplacebo_BUILD cd build && ${libplacebo_COPY} ${libplacebo_ENV} ninja)
 set(libplacebo_INSTALL cd build && ${libplacebo_ENV} ninja install)
 
