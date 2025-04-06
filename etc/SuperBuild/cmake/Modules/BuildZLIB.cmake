33e5da4c198bee5632a70908659bee7f0a08a68e
Gonzalo Garramuño <ggarra13@gmail.com>
2025-04-05 22:12:37 -0300

    Windows ZLIB patch.

--- etc/SuperBuild/cmake/Modules/BuildZLIB.cmake
+++ etc/SuperBuild/cmake/Modules/BuildZLIB.cmake
@@ -3,6 +3,12 @@ include(ExternalProject)
 set(ZLIB_GIT_REPOSITORY "https://github.com/madler/zlib.git")
 set(ZLIB_GIT_TAG "v1.3.1")
 
+if(WIN32)
+    list(APPEND ZLIB_PATCH COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        ${CMAKE_CURRENT_SOURCE_DIR}/ZLIB-patch/zconf.h.cmakein
+        ${CMAKE_CURRENT_BINARY_DIR}/ZLIB/src/ZLIB/zconf.h.cmakein)
+endif()
+
 set(ZLIB_ARGS
     -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
     -DBUILD_SHARED_LIBS=ON
